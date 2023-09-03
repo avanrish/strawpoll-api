126292a6175cd00c3824db1b97d882725fe208d7
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-09-03 13:29:32 +0200

    chore: basic server configuration

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 import { logger } from './logging.middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix('v1', {
     exclude: [''],
